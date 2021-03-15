5a0ee3c6209c684dcb309f7fc4b8e5a0ea471714
Luis Alho <luisalhopt@gmail.com>
2021-03-15 23:58:02 +0000

    Added colors to text

--- go/fizz_buzz/main.go
+++ go/fizz_buzz/main.go
@@ -4,13 +4,33 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("Fizz Buzz Game")
+	/* COLORS
+
+		colorReset := "\033[0m"
+
+	    colorRed := "\033[31m"
+	    colorGreen := "\033[32m"
+	    colorYellow := "\033[33m"
+	    colorBlue := "\033[34m"
+	    colorPurple := "\033[35m"
+	    colorCyan := "\033[36m"
+	    colorWhite := "\033[37m"
+
+	*/
+
+	// Colors to aply to text
+	colorReset := "\033[0m" //Reset the color to original one
+	colorRed := "\033[31m"  //Red
+	colorBlue := "\033[34m" //Blue
+
+	fmt.Println(string(colorRed), "Fizz Buzz Game")
 
 	/*
-	** TODO: Scanner for a number*/
+	** TODO: Scanner for a number
+	 */
 
 	var x int
-	fmt.Println("Please choose a number: ")
+	fmt.Println(string(colorBlue), "Please choose a number: ", string(colorReset))
 
 	x = 10
 	for i := 0; i < x; i++ {
